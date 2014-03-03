47649888377a02512ffe2a8d5ea3fccc900c0197
Graham Markall <graham@opengamma.com>
2014-03-03 20:07:16 +0000

    Add example 6

--- examples/fputil.h
+++ examples/fputil.h
@@ -18,3 +18,22 @@ typedef union {
   double dbl;
 } bitdouble;
 
+/**
+ * Positive infinity
+ */
+
+double get_pos_inf()
+{
+  bitdouble pos_inf_bd = { .bits = 0x7FF0000000000000 };
+  return pos_inf_bd.dbl;
+}
+
+/**
+ * Negative infinity
+ */
+
+double get_neg_inf()
+{
+  bitdouble neg_inf_bd = { .bits = 0xFFF0000000000000 };
+  return neg_inf_bd.dbl;
+}
